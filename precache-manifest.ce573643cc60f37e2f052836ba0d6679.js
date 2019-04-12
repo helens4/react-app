efd65718271a02e61538d5c5ff5fecfa6f773cf2
rafal j <80rass@gmail.com>
2019-04-12 16:56:09 +0200

    Updates

---
+++ precache-manifest.ce573643cc60f37e2f052836ba0d6679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "0dece484ba715f349f36",
+    "url": "/react-app/static/js/main.3eae3e4e.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "0dece484ba715f349f36",
+    "url": "/react-app/static/css/main.9fe4f5a6.chunk.css"
+  },
+  {
+    "revision": "e3fc7b9db01ccb4332604714aa65bc9c",
+    "url": "/react-app/index.html"
+  }
+];
